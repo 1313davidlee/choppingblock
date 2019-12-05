aa33fcb7e471af8d5b38c3553dc4534b638703b0
Olavamo <32780510+Olavamo@users.noreply.github.com>
2019-12-04 21:31:49 -0600

    major facelift

--- vertical-timeline-master/assets/js/index.js
+++ vertical-timeline-master/assets/js/index.js
@@ -1,6 +1,7 @@
 var current_data;
 
 function renderCustom(which_data){
+  // console.log('renderCustom in index.js');
   var personIconBefore = document.getElementById('personIconBefore').innerHTML;
   var personIconAfter = document.getElementById('personIconAfter').innerHTML;
 
@@ -28,25 +29,25 @@ var top_set = {
   'J' : new Array(100)
 
 };
-top_set['A'].fill(0,0,10)
+top_set['A'].fill(0,0,100)
     
-top_set['B'].fill(0,0,20)
+top_set['B'].fill(0,0,100)
 
-top_set['C'].fill(0,0,30)
+top_set['C'].fill(0,0,100)
 
-top_set['D'].fill(0,0,40)
+top_set['D'].fill(0,0,94)
 
-top_set['E'].fill(0,0,50)
+top_set['E'].fill(0,0,83)
 
-top_set['F'].fill(0,0,60)
+top_set['F'].fill(0,0,54)
 
-top_set['G'].fill(0,0,70)
+top_set['G'].fill(0,0,37)
 
-top_set['H'].fill(0,0,80)
+top_set['H'].fill(0,0,29)
 
-top_set['I'].fill(0,0,90)
+top_set['I'].fill(0,0,22)
 
-top_set['J'].fill(0,0,100)
+top_set['J'].fill(0,0,19)
 
 
 
@@ -92,13 +93,58 @@ function edit_top_data() {
   // return top_set[year_pair[given_year]];
 }
 
+function getNum() {
+
+  var given_year = document.querySelector('output').innerHTML;
+
+  if (given_year == 2010) {
+    return 28;
+  }
+  else if(given_year == 2011) {
+    return 29;
+  }
+  else if(given_year == 2012) {
+    return 29;
+  }
+  else if(given_year == 2013) {
+    return 33;
+  }
+  else if(given_year == 2014) {
+    return 37;
+  }
+  else if(given_year == 2015) {
+    return 50;
+  }
+  else if(given_year == 2016) {
+    return 70;
+  }
+  else if(given_year == 2017) {
+    return 84;
+  }
+  else if(given_year == 2018) {
+    return 95;
+  }
+  else if(given_year == 2019) {
+    return 100;
+  }
+
+
+
+  // console.log(top_set.year_pair[given_year]);
+
+  // return top_set[year_pair[given_year]];
+}
+
+
 
 function initialRenderTop(){
   var personIconBefore_top = document.getElementById('personIconBefore_top').innerHTML;
   var personIconAfter_top = document.getElementById('personIconAfter_top').innerHTML;
 
+  // console.log('hello');
+
   document.getElementById('populationBlock_top').innerHTML = "";
-  for (i = 0; i < 100; i++){
+  for (i = 0; i < 28; i++){
 
     if(top_set.A[i] == 0){
       document.getElementById('populationBlock_top').innerHTML += personIconBefore_top
@@ -117,7 +163,10 @@ function renderTop(){
   // console.log(top_set.A);
 
   document.getElementById('populationBlock_top').innerHTML = "";
-  for (i = 0; i < 100; i++){
+
+  var num_people = getNum();
+
+  for (i = 0; i < num_people; i++){
     var pulled_data = edit_top_data();
     // pulled_data = top_set.A
     // console.log(pulled_data);
@@ -147,21 +196,21 @@ function findData(which_data){
     'cs2' : new Array(100),
     'cs3' : new Array(100),
     'cs4' : new Array(100), 
-    'cs5' : new Array(100),
-    'cs6' : new Array(100)
+    // 'cs5' : new Array(100),
+    // 'cs6' : new Array(100)
 
   }
-    d_set['cs1'].fill(0,0,96)
+    d_set['cs1'].fill(0,0,100)
     
-    d_set['cs2'].fill(0,0,65)
+    d_set['cs2'].fill(0,0,98)
     
-    d_set['cs3'].fill(0,0,47)
+    d_set['cs3'].fill(0,0,95)
 
-    d_set['cs4'].fill(0,0,8)
+    d_set['cs4'].fill(0,0,65)
 
-    d_set['cs5'].fill(0,0,2)
+    // d_set['cs5'].fill(0,0,90)
 
-    d_set['cs6'].fill(0,0,0)
+    // d_set['cs6'].fill(0,0,65)
 
   return d_set[which_data]
 }
@@ -175,9 +224,37 @@ function findText(which_data){
     'event2' : 'One driver can operating multiple trucks trailing behind. <br>Result: 75% of the 3.5 million truck drivers without work.', 
     'event3' : 'Trucking companies are the first to incorporate level 5 autonomous driving. Truck drivers are completely out of work.', 
     'event4' : '370,000 personal drivers are out of work. Only a few premium chauffeur services remain.', 
-    'event5' : 'Fully autonomous vehicles penetrate consumer market. Fewer people choose to own personal vehicles.', 
-    'event6' : 'Data shows higher risk of accident with humans behind the wheel. Certain states take actions to ban driving.', 
+    // 'event5' : 'Fully autonomous vehicles penetrate consumer market. Fewer people choose to own personal vehicles.', 
+    // 'event6' : 'Data shows higher risk of accident with humans behind the wheel. Certain states take actions to ban driving.', 
   }
+
+  return d_set[which_data]
+}
+
+function findPrice(which_data){
+  // console.log('findPrice');
+  var d_set = {
+    'event1' : '$650,000', 
+    'event2' : '$900,000 <span style="font-size: 13px; color:green;"> (+$350,000)</span>', 
+    'event3' : '$910,000 <span style="font-size: 13px; color:green;"> (+$10,000)</span>', 
+    'event4' : '$136,000 <span style="font-size: 13px; color:red;"> (-$774,000)</span>', 
+    // 'event5' : 'Fully autonomous vehicles penetrate consumer market. Fewer people choose to own personal vehicles.', 
+    // 'event6' : 'Data shows higher risk of accident with humans behind the wheel. Certain states take actions to ban driving.', 
+  }
+
+  return d_set[which_data]
+}
+
+  function findYear(which_data){
+    // console.log('findText');
+    var d_set = {
+      'event1' : ' (2011)', 
+      'event2' : ' (2013)', 
+      'event3' : ' (2014)', 
+      'event4' : ' (2019)', 
+      // 'event5' : 'Fully autonomous vehicles penetrate consumer market. Fewer people choose to own personal vehicles.', 
+      // 'event6' : 'Data shows higher risk of accident with humans behind the wheel. Certain states take actions to ban driving.', 
+    }
 
   return d_set[which_data]
 }
--- vertical-timeline-master/assets/js/main.js
+++ vertical-timeline-master/assets/js/main.js
@@ -1,139 +1,134 @@
 // import { prependOnceListener } from "cluster";
 
 (function(){
-  // Vertical Timeline - by CodyHouse.co
-	function VerticalTimeline( element ) {
-		this.element = element;
-		this.blocks = this.element.getElementsByClassName("cd-timeline__block");
-		this.images = this.element.getElementsByClassName("cd-timeline__img");
-		this.contents = this.element.getElementsByClassName("cd-timeline__content");
-		this.offset = 0.8;
-		this.hideBlocks();
-	};
-
-	VerticalTimeline.prototype.hideBlocks = function() {
-		if ( !"classList" in document.documentElement ) {
-			return; // no animation on older browsers
-		}
-		//hide timeline blocks which are outside the viewport
-		var self = this;
-		for( var i = 0; i < this.blocks.length; i++) {
-			(function(i){
-				if( self.blocks[i].getBoundingClientRect().top > window.innerHeight*self.offset ) {
-					self.images[i].classList.add("cd-timeline__img--hidden"); 
-					self.contents[i].classList.add("cd-timeline__content--hidden"); 
-				}
-			})(i);
-		}
-		// console.log(this.blocks.length);
-	};
-
-	var event_num_check = 1;
-	var comparison = event_num_check;
-
-	
-
-	VerticalTimeline.prototype.showBlocks = function() {
-		if ( ! "classList" in document.documentElement ) {
-			return;
-		}
-		var self = this;
-
-		for( var i = 0; i < this.blocks.length; i++) {
-			(function(i){
-				if( self.contents[i].classList.contains("cd-timeline__content--hidden") && self.blocks[i].getBoundingClientRect().top <= window.innerHeight*self.offset ) {
-					// add bounce-in animation
-					self.images[i].classList.add("cd-timeline__img--bounce-in");
-					self.contents[i].classList.add("cd-timeline__content--bounce-in");
-
-
-					self.images[i].classList.remove("cd-timeline__img--hidden");
-					self.contents[i].classList.remove("cd-timeline__content--hidden");
-
-					// console.log(event_num_check);
-
-					// if (event_num_check != comparison) {
-					// 	console.log('Trigger');
-					// 	comparison = event_num_check;
-					// }
-
-					event_num_check++;
-				
-				}
-			})(i);
-		}
-	};
-
-	VerticalTimeline.prototype.rightbarUpdate = function() {
-		if ( ! "classList" in document.documentElement ) {
-			return;
-		}
-		var self = this;
-
-		for( var i = 0; i < this.blocks.length; i++) {
-			(function(i){
-				// console.log(i);
-				if (self.blocks[i].getBoundingClientRect().top <= window.innerHeight*self.offset) {
-					document.getElementById("sonnytxt").innerHTML = findText('event' + (i+1));
-					current_data = findData('cs' + (i+1));
-					renderCustom(i);
-				}
-			})(i);
-		}
-	};
-
-	
-
-
-	
-
-	var verticalTimelines = document.getElementsByClassName("js-cd-timeline"),
-		verticalTimelinesArray = [],
-		scrolling = false;
-	if( verticalTimelines.length > 0 ) {
-		for( var i = 0; i < verticalTimelines.length; i++) {
-			(function(i){
-				verticalTimelinesArray.push(new VerticalTimeline(verticalTimelines[i]));
-			})(i);
-		}
-
-		//show timeline blocks on scrolling
-		window.addEventListener("scroll", function(event) {
-			if( !scrolling ) {
-				scrolling = true;
-
-				// console.log();
-
-				(!window.requestAnimationFrame) ? setTimeout(checkTimelineScroll, 250) : window.requestAnimationFrame(checkTimelineScroll);
-			}
-		});
-	}
-
-	initialRenderTop();
-
-	function checkoutput() {
-	let i = document.querySelector('input'),
-		o = document.querySelector('output');
-
-      o.innerHTML = i.value;
-
-      // use 'change' instead to see the difference in response
-      i.addEventListener('input', function () {
+	// Vertical Timeline - by CodyHouse.co
+	  function VerticalTimeline( element ) {
+		  this.element = element;
+		  this.blocks = this.element.getElementsByClassName("cd-timeline__block");
+		  this.images = this.element.getElementsByClassName("cd-timeline__img");
+		  this.contents = this.element.getElementsByClassName("cd-timeline__content");
+		  this.offset = 0.8;
+		  this.hideBlocks();
+	  };
+  
+	  VerticalTimeline.prototype.hideBlocks = function() {
+		  if ( !"classList" in document.documentElement ) {
+			  return; // no animation on older browsers
+		  }
+		  //hide timeline blocks which are outside the viewport
+		  var self = this;
+		  for( var i = 0; i < this.blocks.length; i++) {
+			  (function(i){
+				  if( self.blocks[i].getBoundingClientRect().top > window.innerHeight*self.offset ) {
+					  self.images[i].classList.add("cd-timeline__img--hidden"); 
+					  self.contents[i].classList.add("cd-timeline__content--hidden"); 
+				  }
+			  })(i);
+		  }
+		  // console.log(this.blocks.length);
+	  };
+  
+	  var event_num_check = 1;
+	  var comparison = event_num_check;
+  
+	  
+  
+	  VerticalTimeline.prototype.showBlocks = function() {
+		  if ( ! "classList" in document.documentElement ) {
+			  return;
+		  }
+		  var self = this;
+  
+		  for( var i = 0; i < this.blocks.length; i++) {
+			  (function(i){
+				  if( self.contents[i].classList.contains("cd-timeline__content--hidden") && self.blocks[i].getBoundingClientRect().top <= window.innerHeight*self.offset ) {
+					  // add bounce-in animation
+					  self.images[i].classList.add("cd-timeline__img--bounce-in");
+					  self.contents[i].classList.add("cd-timeline__content--bounce-in");
+  
+  
+					  self.images[i].classList.remove("cd-timeline__img--hidden");
+					  self.contents[i].classList.remove("cd-timeline__content--hidden");
+					  event_num_check++;
+				  
+				  }
+			  })(i);
+		  }
+	  };
+  
+	  VerticalTimeline.prototype.rightbarUpdate = function() {
+		  if ( ! "classList" in document.documentElement ) {
+			  return;
+		  }
+		  var self = this;
+  
+		  for( var i = 0; i < this.blocks.length; i++) {
+			  (function(i){
+				  // console.log(i);
+				  if (self.blocks[i].getBoundingClientRect().top <= window.innerHeight*self.offset) {
+					  document.getElementById("sonnytxt").innerHTML = findText('event' + (i+1));
+					  document.getElementById("medallionPrice").innerHTML = findPrice('event' + (i+1));
+					  document.getElementById("yeartext").innerHTML = findYear('event' + (i+1));
+					  current_data = findData('cs' + (i+1));
+					  renderCustom(i);
+				  }
+			  })(i);
+		  }
+	  };
+  
+	  
+  
+  
+	  
+  
+	  var verticalTimelines = document.getElementsByClassName("js-cd-timeline"),
+		  verticalTimelinesArray = [],
+		  scrolling = false;
+	  if( verticalTimelines.length > 0 ) {
+		  for( var i = 0; i < verticalTimelines.length; i++) {
+			  (function(i){
+				  verticalTimelinesArray.push(new VerticalTimeline(verticalTimelines[i]));
+			  })(i);
+		  }
+  
+		  //show timeline blocks on scrolling
+		  window.addEventListener("scroll", function(event) {
+			  if( !scrolling ) {
+				  scrolling = true;
+  
+				  // console.log();
+  
+				  (!window.requestAnimationFrame) ? setTimeout(checkTimelineScroll, 250) : window.requestAnimationFrame(checkTimelineScroll);
+			  }
+		  });
+	  }
+  
+	  initialRenderTop();
+	//   console.log('main.js');
+  
+	  function checkoutput() {
+	  let i = document.querySelector('input'),
+		  o = document.querySelector('output');
+  
 		o.innerHTML = i.value;
-		edit_top_data();
-		renderTop();
-	  }, false);
-	};
-	
-	checkoutput();
-
-
-	function checkTimelineScroll() {
-		verticalTimelinesArray.forEach(function(timeline){
-			timeline.showBlocks();
-			// console.log('hi');
-			timeline.rightbarUpdate();
-		});
-		scrolling = false;
-	};
-})();
+  
+		// use 'change' instead to see the difference in response
+		i.addEventListener('input', function () {
+		  o.innerHTML = i.value;
+		  edit_top_data();
+		  renderTop();
+		}, false);
+	  };
+	  
+	  checkoutput();
+  
+  
+	  function checkTimelineScroll() {
+		  verticalTimelinesArray.forEach(function(timeline){
+			  timeline.showBlocks();
+			//   console.log('hi');
+			  timeline.rightbarUpdate();
+		  });
+		  scrolling = false;
+	  };
+  })();
